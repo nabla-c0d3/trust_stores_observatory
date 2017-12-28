0dd9b09dd52a013dfbbecbb05f3f22c7eeead583
Alban Diquet <nabla.c0d3@gmail.com>
2017-12-27 23:09:26 -0800

    Do not write trust stores if no changes have been detected

--- refresh_trust_stores.py
+++ refresh_trust_stores.py
@@ -5,17 +5,32 @@ import os
 
 from trust_stores_observatory.certificates_repository import RootCertificatesRepository
 from trust_stores_observatory.store_fetcher import TrustStoreFetcher
-from trust_stores_observatory.trust_store import PlatformEnum
+from trust_stores_observatory.trust_store import PlatformEnum, TrustStore
 
 # Also pass the local certs repo so it gets updated when fetching the trust stores
 certs_repo = RootCertificatesRepository.get_default()
 
-# For each supported platform, fetch the trust store and write it to a YAML file
+# For each supported platform, fetch the trust store
 store_fetcher = TrustStoreFetcher()
 for platform in PlatformEnum:
     print(f'Refreshing {platform.name}...')
-    store = store_fetcher.fetch(platform, certs_repo)
+    fetched_store = store_fetcher.fetch(platform, certs_repo)
     root_path = os.path.abspath(os.path.dirname(__file__))
-    store_path = Path(root_path) / 'trust_stores' / f'{store.platform.name.lower()}.yaml'
-    with open(store_path, mode='w') as store_file:
-        yaml.dump(store, store_file, encoding=('utf-8'), default_flow_style=False)
+
+    # Compare the existing trust store with the one we fetched
+    has_store_changed = False
+    store_path = Path(root_path) / 'trust_stores' / f'{fetched_store.platform.name.lower()}.yaml'
+    try:
+        existing_store = TrustStore.from_yaml(store_path)
+        if existing_store != fetched_store:
+            has_store_changed = True
+    except FileNotFoundError:
+        # The store does not exist in the repo yet
+        has_store_changed = True
+
+    if has_store_changed:
+        print(f'Detected changes for {platform.name}; updating store...')
+        with open(store_path, mode='w') as store_file:
+            yaml.dump(fetched_store, store_file, encoding='utf-8', default_flow_style=False)
+    else:
+        print(f'No changes detected for {platform.name}')
