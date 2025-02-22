7441c5a860fadccf93bdd125fd783f1aab774830
Alban Diquet <nabla.c0d3@gmail.com>
2025-02-22 15:00:27 +0100

    Add task for linting

--- tasks.py
+++ tasks.py
@@ -1,14 +1,21 @@
 from pathlib import Path
-from invoke import task
+from invoke import task, Context
 
 root_path = Path(__file__).parent.absolute()
 
 
 @task
-def test(ctx):
+def test(ctx: Context) -> None:
     # Run linters
     ctx.run("ruff check")
     ctx.run("mypy trust_stores_observatory")
 
     # Run the test suite
     ctx.run("pytest")
+
+
+@task
+def lint(ctx: Context) -> None:
+    ctx.run("ruff format .")
+    ctx.run("ruff check . --fix")
+    ctx.run("mypy .")
--- trust_stores_observatory/certificate_utils.py
+++ trust_stores_observatory/certificate_utils.py
@@ -1,16 +1,18 @@
-from typing import List
-
 from cryptography.x509 import ObjectIdentifier, Name, Certificate, NameOID
 
 
 class CertificateUtils:
     @staticmethod
-    def _get_names_with_oid(name_field: Name, name_oid: ObjectIdentifier) -> List[str]:
-        return [cn.value for cn in name_field.get_attributes_for_oid(name_oid)]
+    def _get_names_with_oid(name_field: Name, name_oid: ObjectIdentifier) -> list[str]:
+        all_names_values = []
+        for cn in name_field.get_attributes_for_oid(name_oid):
+            assert isinstance(cn.value, str)
+            all_names_values.append(cn.value)
+        return all_names_values
 
     @classmethod
     def _get_name_as_text(cls, name_field: Name) -> str:
-        return ", ".join([f"{attr.rfc4514_string()}={attr.value}" for attr in name_field])
+        return ", ".join([f"{attr.rfc4514_string()}={attr.value}" for attr in name_field])  # type: ignore
 
     @classmethod
     def get_canonical_subject_name(cls, certificate: Certificate) -> str:
